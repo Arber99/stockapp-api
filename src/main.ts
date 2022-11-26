7658c166932d0b913e45091bd6cf1f6335c8e9d9
Arber Iseni <arber-99@live.de>
2022-11-26 15:44:58 +0100

    🚧 Trying out different settings

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['*', 'http://localhost:4200/'],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    optionsSuccessStatus: 200,
   });
   app.useGlobalPipes(
     new ValidationPipe({
