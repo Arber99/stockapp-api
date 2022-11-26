e62139deb52628970ca3da9adbc606fe589ffc4a
Arber Iseni <arber-99@live.de>
2022-11-26 15:57:57 +0100

    🚧 Changed settings

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const allowedOrigins = ['http://localhost:4200'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  },
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.enableCors({ ...corsOptions });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
