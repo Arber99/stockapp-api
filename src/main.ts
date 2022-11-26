5b7bc47779faf26fbbcdb7b39675d148270003e0
Arber Iseni <arber-99@live.de>
2022-11-26 15:37:33 +0100

    🚧 Added localhost to origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: ['*', 'http://localhost:4200/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
     optionsSuccessStatus: 204,
