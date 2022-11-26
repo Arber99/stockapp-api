596d887d48ffe07d2e0207eaf09b08fbb0bc2d21
Arber Iseni <arber-99@live.de>
2022-11-26 16:40:11 +0100

    💡 Commented out mailer module

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,38 +1,38 @@
-import { MailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { Module } from '@nestjs/common';
-// import { MailService } from './mail.service';
-import { join } from 'path';
-import { ConfigService } from '@nestjs/config';
+// import { MailerModule } from '@nestjs-modules/mailer';
+// import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+// import { Module } from '@nestjs/common';
+// // import { MailService } from './mail.service';
+// import { join } from 'path';
+// import { ConfigService } from '@nestjs/config';
 
-@Module({
-  imports: [
-    MailerModule.forRootAsync({
-      useFactory: async (config: ConfigService) => ({
-        transport: {
-          host: config.get('SMTP_URL'),
-          port: 587,
-          secure: false,
-          auth: {
-            user: config.get('GMAIL_EMAIL'),
-            pass: config.get('GMAIL_PASSWORD'),
-          },
-        },
-        defaults: {
-          from: '"583 342 - Stockify Code" <>',
-        },
-        template: {
-          dir: join(__dirname, 'templates'),
-          adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
-          options: {
-            strict: true,
-          },
-        },
-      }),
-      inject: [ConfigService],
-    }),
-  ],
-  // providers: [MailService],
-  // exports: [MailService], // 👈 export for DI
-})
-export class MailModule {}
+// @Module({
+//   imports: [
+//     MailerModule.forRootAsync({
+//       useFactory: async (config: ConfigService) => ({
+//         transport: {
+//           host: config.get('SMTP_URL'),
+//           port: 587,
+//           secure: false,
+//           auth: {
+//             user: config.get('GMAIL_EMAIL'),
+//             pass: config.get('GMAIL_PASSWORD'),
+//           },
+//         },
+//         defaults: {
+//           from: '"583 342 - Stockify Code" <>',
+//         },
+//         template: {
+//           dir: join(__dirname, 'templates'),
+//           adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
+//           options: {
+//             strict: true,
+//           },
+//         },
+//       }),
+//       inject: [ConfigService],
+//     }),
+//   ],
+//   // providers: [MailService],
+//   // exports: [MailService], // 👈 export for DI
+// })
+// export class MailModule {}
