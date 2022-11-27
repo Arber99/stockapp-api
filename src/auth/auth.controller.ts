08c54aec0241420be97cc602f4e26c54ffe04c52
Arber Iseni <arber-99@live.de>
2022-11-27 21:55:22 +0100

    🔒️ Added guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,21 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { Throttle } from '@nestjs/throttler';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
+import { ThrottlerBehindProxyGuard } from './guard/throttler-behind-proxy.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @UseGuards(ThrottlerBehindProxyGuard)
   @Throttle(1, 600)
   @Post('signup')
   signup(@Body() dto: AuthDto) {
